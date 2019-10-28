a2a8403ebf8d3984f96f570ade8d629144f1b9e4
Dylano <dylano@simdo.nl>
2019-10-28 16:48:52 +0100

    removed a '/' from config causing conflicts

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: 'gatsby-source-strapi',
       options: {
         apiURL: process.env.DEPLOY_URL
-          ? 'https://portfoliocms.herokuapp.com/'
+          ? 'https://portfoliocms.herokuapp.com'
           : 'http://localhost:1337',
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
