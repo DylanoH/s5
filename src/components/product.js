efe9186da86fdbe6c61ba950a72114958fc90926
Dylano <dylano@simdo.nl>
2019-12-04 17:40:25 +0100

    added colored by category

--- src/components/product.js
+++ src/components/product.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import styled from 'styled-components'
+import React from "react"
+import { Link, graphql, StaticQuery } from "gatsby"
+import styled from "styled-components"
 
-const blue = '#2193B0'
-const lightblue = '#6DD5ED'
+//onderzoek
+const blue = "#2193B0"
+const lightblue = "#6DD5ED"
+
+//beheer
+const pink = "#BF227A"
+const lightpink = "#F44FBF"
+
+//advies
+const purple = "#6401FF"
+const lightpurple = "#B029D8"
+
+//realisatie
+const orange = "#F12711"
+const lightorange = "#F5AF19"
+
+//ontwerp
+const green = "#56AB2F"
+const lightgreen = "#A8E063"
 
 const List = styled.ul`
   list-style: none;
@@ -19,7 +36,7 @@ const Item = styled.li`
     align-items: center;
 
     color: white;
-    background: linear-gradient(90deg, ${blue} 0%, ${lightblue} 100%);
+    /* background: linear-gradient(90deg, ${blue} 0%, ${lightblue} 100%); */
     width: 210px;
     height: 90px;
     margin: 20px 0 0 0;
@@ -30,19 +47,36 @@ const Item = styled.li`
   }
 `
 
-const Product = (props) => {
+const ConditionalItem = styled(Item)`
+  a {
+    ${props =>
+      (props.category === `analyse` &&
+        `background: linear-gradient(90deg, ${blue} 0%, ${lightblue} 100%);`) ||
+      (props.category === `beheer` &&
+        `background: linear-gradient(90deg, ${pink} 0%, ${lightpink} 100%);`) ||
+      (props.category === `advies` &&
+        `background: linear-gradient(90deg, ${purple} 0%, ${lightpurple} 100%);`) ||
+      (props.category === `realisatie` &&
+        `background: linear-gradient(90deg, ${orange} 0%, ${lightorange} 100%);`) ||
+      (props.category === `ontwerp` &&
+        `background: linear-gradient(90deg, ${green} 0%, ${lightgreen} 100%);`)}
+  }
+`
+
+const Product = props => {
   return (
     <StaticQuery
       query={query}
-      render={(data) => (
+      render={data => (
         <List>
-          {data.allStrapiProductlist.edges.map((document) =>
-            document.node.products.map((product) => {
+          {data.allStrapiProductlist.edges.map(document =>
+            document.node.products.map(product => {
               if (product.deelvraag == props.deelvraag) {
+                console.log(product.category)
                 return (
-                  <Item key={product.id}>
+                  <ConditionalItem category={product.category} key={product.id}>
                     <Link to={`/Product_${product.id}`}>{product.titel}</Link>
-                  </Item>
+                  </ConditionalItem>
                 )
               }
             })
