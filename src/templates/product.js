2ea8709abb5af9b6483bf3e2280fe5c20a71628f
Dylano <dylano.hartman@gmail.com>
2019-12-10 00:59:27 +0100

    back button

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import Img from 'gatsby-image'
-import ProductLayout from '../components/productlayout'
-import ReactMarkdown from 'react-markdown'
-import styled from 'styled-components'
+import React from "react"
+import { Link, graphql, StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import ProductLayout from "../components/productlayout"
+import ReactMarkdown from "react-markdown"
+import styled from "styled-components"
 
 const ProductPageSection = styled.section`
   grid-column: 2;
@@ -17,34 +17,42 @@ const ProductPageSection = styled.section`
 const BackButton = styled.div`
   grid-column: 1;
   grid-row: 1;
-  background-color: blue;
   .button_back {
     display: flex;
     width: 100%;
     height: 100%;
+
+    justify-content: center;
+    align-items: center;
+
+    a {
+      width: 100%;
+    }
   }
 `
 
 const ProductTemplate = ({ data }) => (
   <ProductLayout>
     <BackButton>
-      <Link className='button_back' to={`/`} />
+      <Link className="button_back" to={`/`}>
+        <Img fluid={data.file.childImageSharp.fluid}></Img>
+      </Link>
     </BackButton>
     <ProductPageSection>
       <h1>{data.strapiProduct.titel}</h1>
       <ReactMarkdown
-        className='markdown_text'
+        className="markdown_text"
         source={data.strapiProduct.detail}
       />
       <a href={data.strapiProduct.link}>
         <p>zie product hier</p>
       </a>
       <Img
-        className='image_product'
+        className="image_product"
         fixed={data.strapiProduct.preview.childImageSharp.fixed}
       />
       <ReactMarkdown
-        className='markdown_text'
+        className="markdown_text"
         source={data.strapiProduct.reflection}
       />
     </ProductPageSection>
@@ -66,6 +74,15 @@ export const query = graphql`
           fixed(width: 500, height: 500) {
             ...GatsbyImageSharpFixed
           }
+        }
+      }
+    }
+    file(relativePath: { eq: "back.png" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 150) {
+          ...GatsbyImageSharpFluid
         }
       }
     }
